fix exp numerical instaility and delete a exp in guss_decoy func

graph_transformer_edge_layer.py
@@ -26,13 +26,14 @@ def scaling(field, scale_constant):
     return func
 
 # Improving implicit attention scores with explicit edge features, if available
-def imp_exp_attn(implicit_attn, explicit_edge):
+def edge_bias(implicit_attn, explicit_edge):
     """
         implicit_attn: the output of K Q
         explicit_edge: the explicit edge features
     """
     def func(edges):
-        return {implicit_attn: (edges.data[implicit_attn].sum(-1, keepdim=True).clamp(-5, 5) + edges.data[explicit_edge])}
+        return {implicit_attn: (edges.data[implicit_attn] + edges.data[explicit_edge])}
+        # return {implicit_attn: (edges.data[implicit_attn].sum(-1, keepdim=True).clamp(-5, 5) + edges.data[explicit_edge])}
     return func
 
 # To copy edge features to be passed to FFN_e
@@ -45,15 +46,16 @@ def out_edge_features(edge_feat):
 def exp(field):
     def func(edges):
         # clamp for softmax numerical stability
-         return {field: torch.exp((edges.data[field].sum(-1, keepdim=True).clamp(-5, 5)))}
+         return {field: torch.exp((edges.data[field]))}
         # return {field: torch.exp((edges.data[field].sum(-1, keepdim=True)).clamp(-5, 5))}
     return func
 
 # for global decoy func
-def dot_exp(field,adj,rel_pos):
+def guss_decoy(field,adj,rel_pos):
     def func(edges):
         # print()
-        return {field: torch.exp((edges.data[field].sum(-1, keepdim=True).clamp(-5, 5))*edges.data[adj].unsqueeze(1)) + edges.data[rel_pos].unsqueeze(-1)}
+        return {field: edges.data[field].sum(-1, keepdim=True)*edges.data[adj].unsqueeze(1) + edges.data[rel_pos].unsqueeze(-1)}
+        # return {field: torch.exp((edges.data[field].sum(-1, keepdim=True).clamp(-5, 5))*edges.data[adj].unsqueeze(1)) + edges.data[rel_pos].unsqueeze(-1)}
     return func
 def partUpdataScore(out_filed,in_filed,graph_sparse):
     def funv(edges):
@@ -101,7 +103,7 @@ class MultiHeadAttentionLayer(nn.Module):
         # scaling
         # 想个办法吧稀疏图的边上的权重传过来
         full_g.apply_edges(scaling('score', np.sqrt(self.out_dim)))
-        full_g.apply_edges(dot_exp('score', 'adj2','rel_pos_3d'))# distance decay
+        full_g.apply_edges(guss_decoy('score', 'adj2','rel_pos_3d'))# distance decay
         ##########################################
         # 把这个注意力系数映射到稀疏边上
         src,dst = g.edges() # get sparse edges
@@ -109,12 +111,16 @@ class MultiHeadAttentionLayer(nn.Module):
         g.edata['e_out'] = self.attn_proj(g.edata['score'].view(-1,self.num_heads).contiguous()) # score to edge feas
         ############### local module start ################################
         # Compute attention score
-        g.apply_edges(imp_exp_attn('score', 'proj_e'))  # add edge bias 
+        g.apply_edges(edge_bias('score', 'proj_e'))  # add edge bias 
         # 给全图加上edge_bias
         full_g.apply_edges(func=partUpdataScore('score','score',g),edges=g.edges())
         # Copy edge features as e_out to be passed to FFN_e
         # softmax
+        # for softmax numerical stability
+        full_g.edata['score'] -= torch.max(full_g.edata['score'], axis=-1, keepdims=True).values
         full_g.apply_edges(exp('score')) # not div 
+
+        # add a attn dropout
         # Send weighted values to target nodes
         eids = full_g.edges()
         full_g.send_and_recv(eids, fn.src_mul_edge('V_h', 'score', 'V_h'), fn.sum('V_h', 'wV'))

utils.py
@@ -401,9 +401,20 @@ def getToyKey(train_keys):
 
     # train_keys_toy_a[:300] + train_keys_toy_d[:(max_all-300)]
     return train_keys_toy_a[:300] + train_keys_toy_d[:(max_all-300)]
-
+def getTestedPro(file_name):
+    if os.path.exists(file_name):
+        with open(file_name) as f:
+            lines = []
+            for line in f.readlines():
+                if 'actions' in line:
+                    lines.append(line)
+        lines = [line.split('\t')[0] for line in lines]
+        return lines
+    else:
+        return []
 def getEF(model,args,test_path,save_path,device,debug,batch_size,A2_limit,loss_fn,rates = 0.01,flag = ''):
         save_file = save_path + '/EF_test' + flag
+        tested_pros = getTestedPro(save_file)
         test_keys = [key for key in os.listdir(test_path) if '.' not in key]
         pros = defaultdict(list)
         for key in test_keys:
@@ -423,9 +434,16 @@ def getEF(model,args,test_path,save_path,device,debug,batch_size,A2_limit,loss_f
         for rate in rates:
             rate_str += str(rate)+ '\t'
         for pro in pros.keys():
-            # try :
+            try :
+                if pro in tested_pros:
+                    if args.ngpu > 1:
+                        dist.barrier()
+                    print('this pro :  %s  is tested'%pro)
+                    continue
                 test_keys_pro = pros[pro]
                 if test_keys_pro is None:
+                    if args.ngpu > 1:
+                        dist.barrier()
                     continue
                 test_dataset = graphformerDataset(test_keys_pro,args, test_path,debug)
                 val_sampler = SequentialDistributedSampler(test_dataset,args.batch_size) if args.ngpu > 1 else None
@@ -435,8 +453,6 @@ def getEF(model,args,test_path,save_path,device,debug,batch_size,A2_limit,loss_f
                 test_losses,test_true,test_pred = evaluator(model,test_dataloader,loss_fn,args,val_sampler)
                 if args.ngpu > 1:
                     dist.barrier()
-
-
                 if args.local_rank == 0:
                     test_auroc,test_adjust_logauroc,test_auprc,test_balanced_acc,test_acc,test_precision,test_sensitity,test_specifity,test_f1 = get_metrics(test_true,test_pred)
                     test_losses = torch.mean(torch.tensor(test_losses,dtype=torch.float)).data.cpu().numpy()
@@ -474,11 +490,13 @@ def getEF(model,args,test_path,save_path,device,debug,batch_size,A2_limit,loss_f
                         f.write( EF_str + '\t'+str(test_auroc)+ '\t'+str(test_adjust_logauroc)+ '\t'+str(test_auprc)+ '\t'+str(test_balanced_acc)+ '\t'+str(test_acc)+ '\t'+str(test_precision)+ '\t'+str(test_sensitity)+ '\t'+str(test_specifity)+ '\t'+str(test_f1) +'\t'+ str(end-st)+ '\n')
                         f.close()
                     EFs.append(EF)
-            # except:
-            #     print(pro,':skip for some bug')
-            #     continue
+            except:
+                print(pro,':skip for some bug')
                 if args.ngpu > 1:
                     dist.barrier()
+                continue
+            if args.ngpu > 1:
+                dist.barrier()
         if args.local_rank == 0:
             EFs = list(np.sum(np.array(EFs),axis=0)/len(EFs))
             EFs_str = '['